ebe660cb98a6c97aea60dbed1fa694701f472c19
Shourai <10200748+Shourai@users.noreply.github.com>
2024-02-12 13:31:19 +0100

    Add solution for problem 0451

---
+++ 0451_sort_characters_by_frequency.py
@@ -0,0 +1,36 @@
+class Solution:
+    def frequencySort(self, s: str) -> str:
+        counter = {}
+
+        for ch in s:
+            if ch not in counter:
+                counter[ch] = 1
+            else:
+                counter[ch] += 1
+        sorted_counter = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+        lst = []
+        for tuple in sorted_counter:
+            lst.append(tuple[1] * tuple[0])
+
+        return "".join(lst)
+
+
+def case_one():
+    input = "tree"
+    output = "eetr"
+    assert (Solution().frequencySort(input)) == output
+
+
+def case_two():
+    pass
+
+
+def case_three():
+    pass
+
+
+if __name__ == "__main__":
+    case_one()
+    case_two()
+    case_three()
