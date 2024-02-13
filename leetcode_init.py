3eda2a14442bf374640a4e34fe6fdd2793517598
Shourai <10200748+Shourai@users.noreply.github.com>
2024-02-13 10:58:47 +0100

    Update init script to include code snippet from the question

--- leetcode_init.py
+++ leetcode_init.py
@@ -1,25 +1,63 @@
-import os
 import sys
+import requests
+
 
 # Get first argument
 argv = sys.argv[1]
 
-# Split the argument in the number and the name
-string = argv.split(" ")
+# Get the problem name
+slug = argv.split("/")[4]
+
+headers = {
+    "Accept": "*/*",
+    "Accept-Language": "en-US,en;q=0.5",
+    "content-type": "application/json",
+}
+
+json_data = {
+    "query": "\n    query questionEditorData($titleSlug: String!) {\n  question(titleSlug: $titleSlug) {\n    questionId\n    questionFrontendId\n    codeSnippets {\n      lang\n      langSlug\n      code\n    } }\n }\n    ",
+    "variables": {
+        "titleSlug": f"{slug}",
+    },
+    "operationName": "questionEditorData",
+}
+
+response = requests.post(
+    "https://leetcode.com/graphql/", headers=headers, json=json_data
+)
+
+r = response.json()
+code_snippet = r["data"]["question"]["codeSnippets"][3]["code"]
+
 
 # Format number into 4 digit format
-problem_number = string[0]
-problem_number = problem_number.rstrip(".")
+problem_number = r["data"]["question"]["questionId"]
 problem_number = f"{int(problem_number):04}"
 
 # Lowercase the name and change spaces to _
-problem_name = ""
+problem_name = slug.replace("-", "_")
 
-for word in string[1:]:
-    problem_name += "_" + word.lower()
+filename = problem_number + "_" + problem_name + ".py"
 
-# Add extension
-problem_name += ".py"
+test_cases = """
+def case_one():
+    pass
 
-# Create file
-os.system(f"touch {problem_number}{problem_name}")
+
+def case_two():
+    pass
+
+
+def case_three():
+    pass
+
+
+if __name__ == "__main__":
+    case_one()
+    case_two()
+    case_three()
+"""
+
+with open(filename, "w+") as f:
+    f.write(code_snippet)
+    f.write(test_cases)
