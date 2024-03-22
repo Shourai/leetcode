e3bbbfe7e8d3e423732da07855c8e4de887b07fb
Shourai <10200748+Shourai@users.noreply.github.com>
2024-03-22 14:08:34 +0100

    Add log(n) solution for problem 0704

--- 0704_binary_search.py
+++ 0704_binary_search.py
@@ -1,30 +1,56 @@
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
         output = -1
-        if target in nums:
-            output = nums.index(target)
+        n = len(nums)
+        L = 0
+        R = n - 1
+
+        while L <= R:
+            m = (L + R) // 2
+            print(m)
+            if nums[m] < target:
+                L = m + 1
+            elif nums[m] > target:
+                R = m - 1
+            elif nums[m] == target:
+                return m
         return output
 
-        
+
 def case_one():
-    nums = [-1,0,3,5,9,12]
+    nums = [-1, 0, 3, 5, 9, 12]
     target = 9
     output = 4
-    assert(Solution().search(nums, target)) == output
+    print(Solution().search(nums, target))
+    assert (Solution().search(nums, target)) == output
 
 
 def case_two():
-    nums = [-1,0,3,5,9,12]
+    nums = [-1, 0, 3, 5, 9, 12]
     target = 2
     output = -1
-    assert(Solution().search(nums, target)) == output
+    print(Solution().search(nums, target))
+    assert (Solution().search(nums, target)) == output
 
 
 def case_three():
-    pass
+    nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    target = 9
+    output = 8
+    print(Solution().search(nums, target))
+    assert (Solution().search(nums, target)) == output
+
+
+def case_four():
+    nums = [5]
+    target = 5
+    output = 0
+    print(Solution().search(nums, target))
+    assert (Solution().search(nums, target)) == output
 
 
 if __name__ == "__main__":
     case_one()
     case_two()
     case_three()
+    case_four()
