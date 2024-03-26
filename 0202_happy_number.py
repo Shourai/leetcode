d106ede97d72cc23ddc6c34f74726c04efaed5a8
Shourai <10200748+Shourai@users.noreply.github.com>
2024-03-26 15:37:37 +0100

    Add while loop solution to problem 0202

--- 0202_happy_number.py
+++ 0202_happy_number.py
@@ -1,15 +1,31 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
-        digits = list(str(n))
-        n = sum([pow(int(digit),2) for digit in digits])
-        if n == 1:
-            return True
-        elif n == 4:
-            return False
-        else:
-            return self.isHappy(n)
+        seen = []
+
+        while n != 1:
+            digits = list(str(n))
+            n = sum([pow(int(digit), 2) for digit in digits])
+
+            if n in seen:
+                return False
+
+            seen.append(n)
+
+        return True
 
 
+
+
+# class Solution:
+#     def isHappy(self, n: int) -> bool:
+#         digits = list(str(n))
+#         n = sum([pow(int(digit),2) for digit in digits])
+#         if n == 1:
+#             return True
+#         elif n == 4:
+#             return False
+#         else:
+#             return self.isHappy(n)
 
 
 def case_one():
@@ -26,9 +42,11 @@ def case_three():
     n = 1
     assert Solution().isHappy(n) is True
 
+
 def case_four():
     n = 13
     assert Solution().isHappy(n) is True
+
 
 if __name__ == "__main__":
     case_one()
