0785fc28cb94d5ebdff40a7f21adc4c712361e95
Shourai <10200748+Shourai@users.noreply.github.com>
2024-03-26 15:13:06 +0100

    Add solution to problem 0202

---
+++ 0202_happy_number.py
@@ -0,0 +1,37 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        digits = list(str(n))
+        n = sum([pow(int(digit),2) for digit in digits])
+        if n == 1:
+            return True
+        elif n == 4:
+            return False
+        else:
+            return self.isHappy(n)
+
+
+
+
+def case_one():
+    n = 19
+    assert Solution().isHappy(n) is True
+
+
+def case_two():
+    n = 2
+    assert Solution().isHappy(n) is False
+
+
+def case_three():
+    n = 1
+    assert Solution().isHappy(n) is True
+
+def case_four():
+    n = 13
+    assert Solution().isHappy(n) is True
+
+if __name__ == "__main__":
+    case_one()
+    case_two()
+    case_three()
+    case_four()
