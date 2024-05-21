4e26d6c800a32ced9b2a7d93cc9a0a8fde0754d3
Shourai <10200748+Shourai@users.noreply.github.com>
2024-05-21 16:14:02 +0200

    Add function to create linked lists

--- 0206_reverse_linked_list.py
+++ 0206_reverse_linked_list.py
@@ -9,17 +9,42 @@ class ListNode:
         self.next = next
 
 
+def to_linked_list(iterable):
+    head = None
+    for val in reversed(iterable):
+        head = ListNode(val, head)
+    return head
+
+
+def to_native_list(head):
+    lst = []
+    while head:
+        lst.append(head.val)
+        head = head.next
+    return lst
+
+
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         curr = head
         prev = None
 
         while curr is not None:
-            next = curr.next 
+            next = curr.next
             curr.next = prev
             prev = curr
             curr = next
 
+
+h = to_linked_list([1, 2, 4])
+
+print(h)
+print(h.val)
+c = h.next
+print(c.val)
+c = c.next
+print(c.val)
+c = c.next
 
 
 def case_one():
